8524286d378046254275b0ce6274ea111f5b9339
TanKittipat <kittipat12tan@gmail.com>
2024-12-28 18:13:37 +0700

    Connect DB

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+const frontend = process.env.BASE_URL;
+
+app.use(cors({ origin: frontend, credentials: true }));
+// make app can read json.
+app.use(express.json());
+
+// connect to mongodb.
+try {
+  mongoose.connect(uri);
+  console.log("MongoDB connected");
+} catch (error) {
+  console.log("An error occurred while connect to DB: ", error);
+}
+
+app.get("/", (req, res) => {
+  res.send("<h1>Server is ready</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: http://localhost:${port}`);
+});
