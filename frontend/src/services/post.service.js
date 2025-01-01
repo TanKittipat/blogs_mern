153de0d6f21836efe0343f8de8c74990769cd7c0
TanKittipat <kittipat12tan@gmail.com>
2025-01-01 20:33:20 +0700

    Create post services and editor

---
+++ frontend/src/services/post.service.js
@@ -0,0 +1,32 @@
+import api from "./api";
+const postUrl = import.meta.env.VITE_BASE_URL + "posts";
+
+const getAllPosts = async () => {
+  return await api.get(postUrl);
+};
+
+const getPostById = async (id) => {
+  return await api.get(postUrl + `/${id}`);
+};
+
+const createPost = async (post) => {
+  return await api.post(postUrl, post);
+};
+
+const editPost = async (id, post) => {
+  return await api.put(postUrl + `/${id}`, post);
+};
+
+const deletePost = async (id) => {
+  return await api.delete(postUrl + `/${id}`);
+};
+
+const PostServices = {
+  getAllPosts,
+  getPostById,
+  createPost,
+  editPost,
+  deletePost,
+};
+
+export default PostServices;
